92b93c8317d38bf86be2c2e91263990f625684c8
Emilyempa <emilypettersson@hotmail.com>
2026-01-07 17:14:11 +0100

    Add email field to userupdaterequest

--- backend/src/main/java/com/doggydaycare/backend/user/UserUpdateRequest.java
+++ backend/src/main/java/com/doggydaycare/backend/user/UserUpdateRequest.java
@@ -3,6 +3,7 @@ package com.doggydaycare.backend.user;
 public record UserUpdateRequest(
     String firstName,
     String lastName,
+    String email,
     String mobileNumber,
     String emergencyContact,
     Boolean enabled
